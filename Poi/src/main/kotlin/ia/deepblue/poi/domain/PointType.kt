7bf43088525dc9be680a631d59fbf395edb8dea0
olile <lehindeque@gmail.com>
2025-01-09 23:53:40 -0300

    add point interest api exercise

---
+++ Poi/src/main/kotlin/ia/deepblue/poi/domain/PointType.kt
@@ -0,0 +1,11 @@
+package ia.deepblue.poi.domain
+
+enum class PointType(title: String) {
+    CAFETERIA("Lanchonete"),
+    POST("Posto"),
+    JEWELRY("Joalheria"),
+    FLORIS("Floricultura"),
+    PUB("Pub"),
+    SUPERMARKET("Supermercado"),
+    STEAKHOUSE("Churrascaria")
+}
---
+++ Poi/src/test/kotlin/ia/deepblue/poi/PoiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ia.deepblue.poi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PoiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
